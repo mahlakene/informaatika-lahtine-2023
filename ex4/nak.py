07e5e756863308ddb038fec2982d53b9062a6a4e
mikeso <mikeso@ttu.ee>
2023-10-12 22:43:45 +0300

    informaatika lahtine

--- ex4/nak.py
+++ ex4/nak.py
@@ -1,9 +1,8 @@
 import sys
 
-
 lines = sys.stdin.readlines()
 
-"""with open("test2", "r") as f:
+"""with open("test3", "r") as f:
     lines = f.readlines()"""
 total_players = int(lines[0].split()[0])  # N
 total_plans = int(lines[0][:-1].split()[1])  # Q
@@ -20,17 +19,17 @@ def counting_sort(array, k):
         count[i] += count[i - 1]
     i = size - 1
     while i >= 0:
-        output[count[array[i]] - 1] = array[i]
-        count[array[i]] -= 1
+        nr = array[i]
+        output[count[nr] - 1] = nr
+        count[nr] -= 1
         i -= 1
-    for i in range(0, min(size, 12)):
+    for i in range(min(size, 12)):
         array[i] = output[i]
     return array
 
-result = ""
+
 for i in range(2, total_plans + 2):
     left_index = int(lines[i].split()[0])
     right_index = int(lines[i][:-1].split()[1]) + 1
     a = ages[left_index:right_index]
-    result = str(sorted(a)[10])
-    print(result)
+    print(counting_sort(a, 57)[10])
--- ex4/test.py
+++ ex4/test.py
@@ -1,7 +1,7 @@
 import random
 
 with open("test2", "w") as f:
-    f.write(f"{2 * (10 ** 5)} {3 * (10 ** 5)}\n")
+    """f.write(f"{2 * (10 ** 5)} {3 * (10 ** 5)}\n")
     second_line = ""
     for i in range(2 * (10 ** 5)):
         second_line += " " + str(random.randint(16, 56))
@@ -10,4 +10,18 @@ with open("test2", "w") as f:
     for i in range(3 * (10 ** 5)):
         a = random.randint(0, 2 * (10 ** 5) - 15)
         b = random.randint(a + 11, 2 * (10 ** 5))
+        f.write(f"{a} {b}\n")"""
+
+
+with open("test3", "w") as f:
+    f.write("1000 1000\n")
+    second_line = ""
+    for i in range(100000):
+        second_line += " " + str(random.randint(16, 56))
+    second_line = second_line[1:] + "\n"
+    f.write(second_line)
+    for i in range(10000):
+        a = random.randint(0, 100000 - 15)
+        b = random.randint(a + 11, 100000)
         f.write(f"{a} {b}\n")
+

11d9ef1276f3d53b77b5f683b8991458f26563ad
mikeso <mikeso@ttu.ee>
2023-10-13 16:52:34 +0300

    informaatika lahtine

--- ex4/test.py
+++ ex4/test.py
@@ -1,7 +1,7 @@
 import random
 
 with open("test2", "w") as f:
-    """f.write(f"{2 * (10 ** 5)} {3 * (10 ** 5)}\n")
+    f.write(f"{2 * (10 ** 5)} {3 * (10 ** 5)}\n")
     second_line = ""
     for i in range(2 * (10 ** 5)):
         second_line += " " + str(random.randint(16, 56))
@@ -10,7 +10,7 @@ with open("test2", "w") as f:
     for i in range(3 * (10 ** 5)):
         a = random.randint(0, 2 * (10 ** 5) - 15)
         b = random.randint(a + 11, 2 * (10 ** 5))
-        f.write(f"{a} {b}\n")"""
+        f.write(f"{a} {b}\n")
 
 
 with open("test3", "w") as f:
